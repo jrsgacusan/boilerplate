c7fd2eeb10c4588f831a33cc8eeda4bcdcbd8b45
Juel Rei S. Gacusan <jgacusan@tacitknowledge.com>
2022-06-01 14:52:14 +0800

    Added support for different env

--- src/typescript/App.tsx
+++ src/typescript/App.tsx
@@ -3,6 +3,9 @@ import Image from "../images/random.png";
 const App = () => {
   return (
     <>
+      <h1>
+        Env: - {process.env.NODE_ENV} {process.env.name}
+      </h1>
       <img src={Image} alt="test" />
       <h1>React test s</h1>
     </>
